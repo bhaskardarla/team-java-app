6344ea63974275ffe7a064130b8835cfcd35988e
bhaskardarla <149381602+bhaskardarla@users.noreply.github.com>
2026-09-09 16:11:39 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,37 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Team Java App!");
-        System.out.println("Version: 1.0");
-        System.out.println("Docker -> ECR -> EC2 deployment successful.");
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8080), 0
+        );
+
+        server.createContext("/", exchange -> {
+
+            String response =
+                    "Hello from Team Java App!<br>" +
+                    "Version: 1.0<br>" +
+                    "Docker -> ECR -> EC2 deployment successful!";
+
+            exchange.getResponseHeaders()
+                    .set("Content-Type", "text/html");
+
+            exchange.sendResponseHeaders(
+                    200, response.getBytes().length
+            );
+
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.close();
+        });
+
+        server.start();
+
+        System.out.println("Application started on port 8080");
     }
 }
