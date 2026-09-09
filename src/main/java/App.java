619496deba2a2659ff35ceaca00bcba3cf325c81
bhaskardarla <149381602+bhaskardarla@users.noreply.github.com>
2026-09-09 15:54:54 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Team Java App!");
+        System.out.println("Version: 1.0");
+        System.out.println("Docker -> ECR -> EC2 deployment successful.");
+    }
+}
