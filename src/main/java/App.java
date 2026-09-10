d26e3753dcca5f16ce4d1e28d329418ba75f4797
bhaskardarla <149381602+bhaskardarla@users.noreply.github.com>
2026-09-10 10:10:55 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,9 @@
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
-
-    public static void main(String[] args) throws IOException {
-
-        HttpServer server = HttpServer.create(
-                new InetSocketAddress(8080), 0
-        );
-
-        server.createContext("/", exchange -> {
-
-            String response =
-                    "Hello from Team Java App!<br>" +
-                    "Version: 1.0<br>" +
-                    "Docker -> ECR -> EC2 deployment successful!";
-
-            exchange.getResponseHeaders()
-                    .set("Content-Type", "text/html");
-
-            exchange.sendResponseHeaders(
-                    200, response.getBytes().length
-            );
-
-            OutputStream output = exchange.getResponseBody();
-            output.write(response.getBytes());
-            output.close();
-        });
-
-        server.start();
-
-        System.out.println("Application started on port 8080");
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
